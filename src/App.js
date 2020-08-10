228c0d8af8880fc497b8e64c34530ef73d47290f
Duc Trung <sductrung@gmail.com>
2020-08-10 23:22:22 +0700

    first submit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            posts: [
+                {title: 'post1', content: 'content 1'},
+                {title: 'post2', content: 'content 2'},
+                {title: 'post3', content: 'content 3'}
+            ],
+            title: '',
+            content: ''
+        }
+    }
+    handleChangeTitle = (event) => {
+        this.setState({title: event.target.value})
+    }
+    handleChangeContent = (event) => {
+        this.setState({content: event.target.value})
+    }
+    handleSubmit = (event) => {
+        event.preventDefault()
+
+        let {title, content } = this.state;
+        this.setState({posts: [...this.state.posts, {title, content}]})
+        this.setState({title:''})
+        this.setState({content: ''})
+    }
+    render() {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-12">
+                        <form onSubmit={this.handleSubmit}>
+                            <label>Search Image:</label>
+                            <input name="title" value={this.state.title} onChange={this.handleChangeTitle}/>
+                        </form>
+                    </div>
+                </div>
+                <div className="row">
+                </div>
+            </div>
+        )
+    }
 }
 
-export default App;
+export default App
